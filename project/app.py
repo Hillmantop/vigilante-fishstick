1045b28983f4869c908711f03052b6d6270cac86
Hill <116369029+Hillmantop@users.noreply.github.com>
2024-01-17 20:06:28 +0200

    Add files via upload

---
+++ project/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, render_template, request
+
+# this version of the app is to see what is happening in the layout.html it is not final
+# in anyway
+
+# Configure application
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
